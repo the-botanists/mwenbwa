8c222fb7f3c47a5dc23a2771f3a1653d14262827
Emi <pieront.emilie@gmail.com>
2021-02-10 15:29:22 +0100

    test db connect

--- src/server/config/dbconnect.js
+++ src/server/config/dbconnect.js
@@ -1,14 +1,38 @@
-const express = require(`express`);
-//bring in mongoose dependency that we installed
-const mongoose = require(`mongoose`);
-//bring in mongo uri from mlab
-const mongoURI =
-    "mongodb+srv://botanistes:kkYos247aWNYxb0j@botanistes.tjvkg.mongodb.net/botanistes?retryWrites=true&w=majority";
-mongoose.connect(mongoURI, {
-    useMongoClient: true,
-});
-const app = express();
-const port = 5000;
-app.listen(port, () => {
-    console.log(`app started on port ${port}`);
-});
+// const express = require(`express`);
+// //bring in mongoose dependency that we installed
+// const mongoose = require(`mongoose`);
+// //bring in mongo uri from mlab
+// const mongoURI =
+//     "mongodb+srv://botanistes:kkYos247aWNYxb0j@botanistes.tjvkg.mongodb.net/botanistes?retryWrites=true&w=majority";
+// mongoose.connect(mongoURI, {
+//     useMongoClient: true,
+// });
+// const app = express();
+// const port = 5000;
+// app.listen(port, () => {
+//     console.log(`app started on port ${port}`);
+// });
+
+// import {insertDocuments} from "../insertdocs";
+
+// const MongoClient = require("mongodb").MongoClient;
+// const assert = require("assert");
+
+// // Connection URL
+// const url =
+//     "mongodb+srv://botanistes:kkYos247aWNYxb0j@botanistes.tjvkg.mongodb.net";
+
+// // Database Name
+// const dbName = "botanistes";
+
+// // Use connect method to connect to the server
+// MongoClient.connect(url, (err, client) => {
+//     assert.equal(null, err);
+//     console.log("Connected successfully to server");
+
+//     const db = client.db(dbName);
+
+//     insertDocuments(db, () => {
+//         client.close();
+//     });
+// });
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@
  * coded by leny@BeCode
  * started at 18/05/2020
  */
-
 /* eslint-disable */
 
 const webpack = require("webpack");
@@ -115,3 +114,5 @@ module.exports = env => {
         watch: env === "dev",
     };
 };
+
+  
