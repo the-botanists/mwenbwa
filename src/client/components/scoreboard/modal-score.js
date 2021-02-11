714813e7e4a36e3f1731fb82595f0fcca4ec0648
KevKsar <cassart.kev@gmail.com>
2021-02-11 22:43:15 +0100

    add & modify table scoreboard

--- src/client/components/scoreboard/modal-score.js
+++ src/client/components/scoreboard/modal-score.js
@@ -7,29 +7,16 @@ import TableScore from "./table-score";
 // import ButtonCloseModal from "./button-close-modal";
 import Button from "./button";
 
-const modalStyle = {
-    position: "absolute",
-    zIndex: 1000,
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    background: "rgba(0, 0, 0, 0.20)",
-};
-
 const ModalScore = ({show = false, onCloseModal, onShareScore}) => {
     if (!show) {
         return null;
     }
 
     return createPortal(
-        <div style={modalStyle}>
-            <div className={classnames("box")}>
+        <div className={classnames("k-modalScore")}>
+            <div className={classnames("box", "k-boxModal")}>
                 <TableScore />
-                <div className={classnames("contButtons")}>
+                <div className={classnames("k-buttonsGroup")}>
                     <Button label={"Close"} onClick={onCloseModal} />
                     <Button label={"Share"} onClick={onShareScore} />
                 </div>
--- src/client/components/scoreboard/table-score.js
+++ src/client/components/scoreboard/table-score.js
@@ -3,7 +3,7 @@ import classnames from "classnames";
 
 const TableScore = () => (
     <>
-        <h4 className={"k-titleScore"}>{"ScoreBoard"}</h4>
+        <div className={"k-titleScore"}>{"ScoreBoard"}</div>
         <table className={"table"}>
             <thead>
                 <tr>
@@ -17,103 +17,35 @@ const TableScore = () => (
                     <th />
                 </tr>
             </thead>
-            <tbody>
-                <tr className={classnames("k-tableRow")}>
-                    <th>{"1"}</th>
-                    <td>{"kevksar"}</td>
-                    <td className={classnames("k-tdScore")}>{"384546"}</td>
-                    <td>
-                        <div className={classnames("k-leafIcon")} />
-                    </td>
-                    <td className={classnames("k-tdScore")}>{"2369"}</td>
-                    <td>
-                        <div className={classnames("k-treeIcon")}> </div>
-                    </td>
-                </tr>
-                <tr className={classnames("k-tableRow", "is-selected")}>
-                    <th>{"2"}</th>
-                    <td>{"kevksar"}</td>
-                    <td className={classnames("k-tdScore")}>{"38456"}</td>
-                    <td>
-                        <div className={classnames("k-leafIcon")} />
-                    </td>
-                    <td className={classnames("k-tdScore")}>{"2139"}</td>
-                    <td>
-                        <div className={classnames("k-treeIcon")}> </div>
-                    </td>
-                </tr>
-                <tr className={classnames("k-tableRow")}>
-                    <th>{"3"}</th>
-                    <td>{"kevksar"}</td>
-                    <td className={classnames("k-tdScore")}>{"384156"}</td>
-                    <td>
-                        <div className={classnames("k-leafIcon")} />
-                    </td>
-                    <td className={classnames("k-tdScore")}>{"2390"}</td>
-                    <td>
-                        <div className={classnames("k-treeIcon")}> </div>
-                    </td>
-                </tr>
-                <tr className={classnames("k-tableRow")}>
-                    <th>{"4"}</th>
-                    <td>{"kevksar"}</td>
-                    <td className={classnames("k-tdScore")}>{"38456"}</td>
-                    <td>
-                        <div className={classnames("k-leafIcon")} />
-                    </td>
-                    <td className={classnames("k-tdScore")}>{"29"}</td>
-                    <td>
-                        <div className={classnames("k-treeIcon")}> </div>
-                    </td>
-                </tr>
-                <tr className={classnames("k-tableRow")}>
-                    <th>{"5"}</th>
-                    <td>{"kevksar"}</td>
-                    <td className={classnames("k-tdScore")}>{"1456"}</td>
-                    <td>
-                        <div className={classnames("k-leafIcon")} />
-                    </td>
-                    <td className={classnames("k-tdScore")}>{"2309"}</td>
-                    <td>
-                        <div className={classnames("k-treeIcon")}> </div>
-                    </td>
-                </tr>
-                <tr className={classnames("k-tableRow")}>
-                    <th>{"6"}</th>
-                    <td>{"kevksar"}</td>
-                    <td className={classnames("k-tdScore")}>{"38456"}</td>
-                    <td>
-                        <div className={classnames("k-leafIcon")} />
-                    </td>
-                    <td className={classnames("k-tdScore")}>{"239"}</td>
-                    <td>
-                        <div className={classnames("k-treeIcon")}> </div>
-                    </td>
-                </tr>
-                <tr className={classnames("k-tableRow")}>
-                    <th>{"7"}</th>
-                    <td>{"kevksar"}</td>
-                    <td className={classnames("k-tdScore")}>{"38456"}</td>
-                    <td>
-                        <div className={classnames("k-leafIcon")} />
-                    </td>
-                    <td className={classnames("k-tdScore")}>{"239"}</td>
-                    <td>
-                        <div className={classnames("k-treeIcon")}> </div>
-                    </td>
-                </tr>
-                <tr className={classnames("k-tableRow")}>
-                    <th>{"8"}</th>
-                    <td>{"kevksar"}</td>
-                    <td className={classnames("k-tdScore")}>{"38456"}</td>
-                    <td>
-                        <div className={classnames("k-leafIcon")} />
-                    </td>
-                    <td className={classnames("k-tdScore")}>{"239"}</td>
-                    <td>
-                        <div className={classnames("k-treeIcon")}> </div>
-                    </td>
-                </tr>
+
+            <tbody classnames={"k-tbodyScore"}>
+                {Array.apply(0, new Array(8)).map((x, i) => {
+                    console.log(i);
+                    let pos = i;
+                    console.log(pos);
+                    return (
+                        <tr
+                            className={classnames("k-tableRow")}
+                            key={i.toString()}>
+                            <th>{++pos}</th>
+                            <td>{"kevksar"}</td>
+                            <td className={classnames("k-tdScore")}>
+                                {Math.floor(Math.random() * 100000 + 1000)}
+                            </td>
+                            <td>
+                                <div className={classnames("k-leafIcon")} />
+                            </td>
+                            <td className={classnames("k-tdScore")}>
+                                {Math.floor(Math.random() * 10000 + 1)}
+                            </td>
+                            <td>
+                                <div className={classnames("k-treeIcon")}>
+                                    {" "}
+                                </div>
+                            </td>
+                        </tr>
+                    );
+                })}
             </tbody>
         </table>
     </>
