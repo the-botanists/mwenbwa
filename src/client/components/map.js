d3d9a3d2e241ad1d3685392e5ccf6b282c89e188
Cedric AUDRIT <cedric@audrit.be>
2021-02-13 21:16:37 +0100

    tree map marker #13 #12
    - use swr for get data from api alltree
    - add marker on map
    - add link to wiki
    - add clusterMarker
    - ad tree map Popup

--- src/client/components/map.js
+++ src/client/components/map.js
@@ -1,12 +1,21 @@
 import React from "react";
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet"; //
+import MarkerClusterGroup from "react-leaflet-markercluster";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "../assets/css/map.css";
+import "react-leaflet-markercluster/dist/styles.min.css";
 //import TreeImage from "../assets/img/arbre.svg";
 import TreeImage from "../assets/img/watercolor-tree1.png";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then(response => response.json());
 
 function GameMap() {
+    const urlAllTree = "/api/trees";
+    const {data, error} = useSWR(urlAllTree, {fetcher});
+    const allTreesData = data && !error ? data.slice() : [];
+
     const position = [50.6283, 5.5768];
     const treeIcon = L.icon({
         iconUrl: TreeImage,
@@ -17,13 +26,46 @@ function GameMap() {
         //shadowSize:   [50, 64], // size of the shadow
         // shadowAnchor: [4, 62],  // the same for the shadow
     });
+    function fixLatinName(treeLatinName) {
+        let latinName = treeLatinName.split(` X `)[0];
+        latinName = latinName.split(` x `)[0];
+        latinName = latinName.split(` '`)[0];
+        return latinName;
+    }
+
+    const markerTree = allTreesData.map(tree => (
+        <Marker
+            key={tree._id.$oid}
+            icon={treeIcon}
+            position={tree.location.coordinates}>
+            <Popup>
+                {"I'm a Tree"}
+                <br />
+                {"More info soon"}
+                <p>
+                    <a
+                        href={`https://en.wikipedia.org/wiki/${fixLatinName(
+                            tree.latinName,
+                        )}`}
+                        target={"_blank"}
+                        rel={"noreferrer"}>
+                        {"Wiki info"}
+                    </a>
+                </p>
+                <button type={"button"} className={"button is-success"}>
+                    {"Success"}
+                </button>
+            </Popup>
+        </Marker>
+    ));
+
     return (
         <MapContainer
             id={"leafletContainer"}
             center={position}
-            zoom={16}
-            minZoom={10}
-            maxZoom={17}
+            zoom={17}
+            minZoom={13}
+            maxZoom={18}
             scrollWheelZoom={true}>
             <TileLayer
                 url={
@@ -31,16 +73,17 @@ function GameMap() {
                 }
                 // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker icon={treeIcon} position={position}>
-                <Popup>
-                    {"I'm a Tree"}
-                    <br />
-                    {"More info soon"}
-                    <button type={"button"} className={"button is-success"}>
-                        {"Success"}
-                    </button>
-                </Popup>
-            </Marker>
+            <MarkerClusterGroup
+                spiderfyOnMaxZoom={false}
+                showCoverageOnHover={false}
+                zoomToBoundsOnClick={false}
+                disableClusteringAtZoom={16}>
+                {markerTree}
+            </MarkerClusterGroup>
+            {/* {allTreesData.map((tree) => (
+                <Marker key={tree._id.$oid} icon={treeIcon} position={tree.location.coordinates}>
+                </Marker>
+            ))} */}
         </MapContainer>
     );
 }
