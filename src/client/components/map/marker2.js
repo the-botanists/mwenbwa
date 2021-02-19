91cf72af9f4536cab3d4b9c395e4b715f8a4e5e5
Cedric AUDRIT <cedric@audrit.be>
2021-02-19 15:21:05 +0100

    limit zone tree

--- src/client/components/map/marker2.js
+++ src/client/components/map/marker2.js
@@ -1,158 +1,142 @@
-// import React, {useState, useEffect} from "react"; // useMap, useMapEvents
-// import axios from "axios";
-// import {Marker, Popup} from "react-leaflet"; // MapContainer, TileLayer
-// import L from "leaflet";
+import React, {useState, useEffect} from "react"; // useMap, useMapEvents
+import axios from "axios";
+import {Marker, Popup} from "react-leaflet"; // MapContainer, TileLayer
+import L from "leaflet";
 // import TreeImage0 from "../../assets/img/watercolor-tree1.png";
-// import TreeImage1 from "../../assets/img/watercolor-tree2.png";
+import TreeImage1 from "../../assets/img/watercolor-tree2.png";
+// import TreeImageSVG from "../../assets/img/watercolor-tree-1.svg";
 
-// const randtreeIcon = () => {
-//     function getRandomInt(max) {
-//         return Math.floor(Math.random() * Math.floor(max));
-//     }
-//     const rand2 = getRandomInt(2);
-//     if (rand2 === 1) {
-//         const treeIcon = L.icon({
-//             iconUrl: TreeImage0,
-//             iconSize: [30, 40], // size of the icon
-//             iconAnchor: [17, 0], // point of the icon which will correspond to marker's location
-//             popupAnchor: [17, 0], // point from which the popup should open relative to the iconAnchor
-//         });
-//         return treeIcon;
-//     }
-//     const treeIcon = L.icon({
-//         iconUrl: TreeImage1,
-//         iconSize: [30, 40], // size of the icon
-//         iconAnchor: [17, 0], // point of the icon which will correspond to marker's location
-//         popupAnchor: [17, 0], // point from which the popup should open relative to the iconAnchor
-//     });
-//     return treeIcon;
-// };
+const treeIcon = L.icon({
+    iconUrl: TreeImage1,
+    iconSize: [30, 40], // size of the icon
+    iconAnchor: [17, 0], // point of the icon which will correspond to marker's location
+    popupAnchor: [17, 0], // point from which the popup should open relative to the iconAnchor
+});
 
-// function fixLatinName(treeLatinName) {
-//     let latinName = treeLatinName.split(` X `)[0];
-//     latinName = latinName.split(` x `)[0];
-//     latinName = latinName.split(` '`)[0];
-//     return latinName;
-// }
+function fixLatinName(treeLatinName) {
+    let latinName = treeLatinName.split(` X `)[0];
+    latinName = latinName.split(` x `)[0];
+    latinName = latinName.split(` '`)[0];
+    return latinName;
+}
 
-// const center = [50.6283, 5.5768];
-// const latMin = center[0] - 0.003;
-// const latMax = center[0] + 0.003;
-// const lonMin = center[1] - 0.007;
-// const lonMax = center[1] + 0.007;
+const curLocation = [50.6283, 5.5768];
+const latMin = center => center[0] - 0.0025;
+const latMax = center => center[0] + 0.0025;
+const lonMin = center => center[1] - 0.004;
+const lonMax = center => center[1] + 0.004;
 
-// function GetMarker() {
-//     const [error, setError] = useState(null);
-//     // const [isLoaded, setIsLoaded] = useState(false);
-//     const [treesmarker, setTreesmarker] = useState([]);
+export const GetMarker = () => {
+    // CurrentCenter, para2
+    const [error, setError] = useState(null);
+    const [treesmarker, setTreesmarker] = useState([]);
+    // const [curLocation, setCurLocation] = useState([]);
 
-//     // console.log(currentZoom);
+    // if (CurrentCenter === null) {
+    //     CurrentCenter = [50.6287, 5.5767];
+    // } else {
+    //     CurrentCenter = Object.values(CurrentCenter);
+    //     console.log("TEST A1:", CurrentCenter[0]);
+    // }
+    // console.log("PAR2 : ", Object.values(para2));
+    // // setCurLocation(CurrentCenter)
+    useEffect(() => {
+        axios
+            .get("/api/trees/all")
+            .then(response => {
+                setTreesmarker(response.data);
+            })
+            // })
+            .catch(() => {
+                // console.log("Error retrieving data!!!");
+                setError(error);
+                console.log(error);
+            });
+    });
 
-//     useEffect(() => {
-//         axios
-//             .get("/api/trees/all")
-//             .then(response => {
-//                 // setIsLoaded(true);
-//                 setTreesmarker(response.data);
-//             })
-//             // })
-//             .catch(() => {
-//                 // console.log("Error retrieving data!!!");
-//                 // setIsLoaded(true);
-//                 setError(error);
-//                 console.log(error);
-//             });
-//     });
+    function buyTree() {
+        console.log("Great Shot!");
+    }
 
-//     // if (!isLoaded) {
-//     //     return <span className={"title"}>{"loading"}</span>;
-//     // }
-//     return treesmarker.map(tree => {
-//         console.log(tree.location.coordinates[0]);
-//         console.log(`ATT :${lonMin}`);
-//         console.log(tree.location.coordinates[1]);
-//         if (
-//             tree.location.coordinates[1] > lonMin &&
-//             tree.location.coordinates[1] < lonMax &&
-//             tree.location.coordinates[0] > latMin &&
-//             tree.location.coordinates[0] < latMax
-//         ) {
-//             return (
-//                 <>
-//                     {/* <MapEvents /> */}
-//                     <Marker
-//                         key={tree._id}
-//                         icon={randtreeIcon()}
-//                         position={tree.location.coordinates}>
-//                         <Popup>
-//                             {"I'm a Tree"}
-//                             <br />
-//                             <p>
-//                                 {"Value : "}
-//                                 {/* {parseInt((tree.circonf / 3.1421) * tree.height)} */}
-//                                 {" leafs"}{" "}
-//                             </p>
-//                             <p>
-//                                 {fixLatinName(tree.latinName)}{" "}
-//                                 <a
-//                                     href={`https://en.wikipedia.org/wiki/${fixLatinName(
-//                                         tree.latinName,
-//                                     )}`}
-//                                     target={"_blank"}
-//                                     rel={"noreferrer"}>
-//                                     {"Wiki info"}
-//                                 </a>
-//                             </p>
-//                             <div className={"control"}>
-//                                 <label className={"label"}>{"latinName"}</label>
-//                                 <input
-//                                     className={"input"}
-//                                     type={"text"}
-//                                     placeholder={"Text input"}
-//                                     value={tree.latinName}
-//                                 />
-//                                 <label className={"label"}>
-//                                     {"coordinates"}
-//                                 </label>
-//                                 <input
-//                                     className={"input"}
-//                                     type={"text"}
-//                                     placeholder={"Text input"}
-//                                     value={tree.location.coordinates}
-//                                 />
-//                                 <label className={"label"}>{"circonf"}</label>
-//                                 <input
-//                                     className={"input"}
-//                                     type={"text"}
-//                                     placeholder={"Text input"}
-//                                     value={tree.circonf}
-//                                 />
-//                                 <label className={"label"}>{"height"}</label>
-//                                 <input
-//                                     className={"input"}
-//                                     type={"text"}
-//                                     placeholder={"Text input"}
-//                                     value={tree.height}
-//                                 />
-//                                 <label className={"label"}>{"Name"}</label>
-//                                 <input
-//                                     className={"input"}
-//                                     type={"text"}
-//                                     placeholder={"Text input"}
-//                                     value={"123"}
-//                                 />
-//                             </div>
-//                             <button
-//                                 type={"button"}
-//                                 className={"button is-success"}>
-//                                 {"Buy"}
-//                             </button>
-//                         </Popup>
-//                     </Marker>
-//                 </>
-//             );
-//         }
-//     });
-// }
+    const TEST = treesmarker
+        .filter(
+            tree =>
+                tree.location.coordinates[1] > lonMin(curLocation) &&
+                tree.location.coordinates[1] < lonMax(curLocation) &&
+                tree.location.coordinates[0] > latMin(curLocation) &&
+                tree.location.coordinates[0] < latMax(curLocation),
+        )
+        .map(tree => (
+            <Marker
+                key={tree._id}
+                icon={treeIcon} // {randtreeIcon()}
+                position={tree.location.coordinates}>
+                <Popup>
+                    {"I'm a Tree"}
+                    <br />
+                    <p>
+                        {"Value : "}
+                        {parseInt((tree.circonf / 3.1421) * tree.height)}
+                        {" leafs"}{" "}
+                    </p>
+                    <p>
+                        {fixLatinName(tree.latinName)}{" "}
+                        <a
+                            href={`https://en.wikipedia.org/wiki/${fixLatinName(
+                                tree.latinName,
+                            )}`}
+                            target={"_blank"}
+                            rel={"noreferrer"}>
+                            {"Wiki info"}
+                        </a>
+                    </p>
+                    <div className={"control"}>
+                        <label className={"label"}>{"latinName"}</label>
+                        <input
+                            className={"input"}
+                            type={"text"}
+                            placeholder={"Text input"}
+                            value={tree.latinName}
+                        />
+                        <label className={"label"}>{"coordinates"}</label>
+                        <input
+                            className={"input"}
+                            type={"text"}
+                            placeholder={"Text input"}
+                            value={tree.location.coordinates}
+                        />
+                        <label className={"label"}>{"circonf"}</label>
+                        <input
+                            className={"input"}
+                            type={"text"}
+                            placeholder={"Text input"}
+                            value={tree.circonf}
+                        />
+                        <label className={"label"}>{"height"}</label>
+                        <input
+                            className={"input"}
+                            type={"text"}
+                            placeholder={"Text input"}
+                            value={tree.height}
+                        />
+                        <label className={"label"}>{"Name"}</label>
+                        <input
+                            className={"input"}
+                            type={"text"}
+                            placeholder={"Text input"}
+                            value={"123"}
+                        />
+                    </div>
+                    <button
+                        type={"button"}
+                        className={"button is-success"}
+                        onClick={buyTree}>
+                        {"Buy"}
+                    </button>
+                </Popup>
+            </Marker>
+        ));
+    return TEST;
+};
 
-// export default GetMarker;
+// export default {GetMarker};
