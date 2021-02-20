007aafedf2cc46f3e61841bfe042cf938b55bd43
KevKsar <cassart.kev@gmail.com>
2021-02-20 23:46:05 +0100

    up > colorpicker

--- src/client/components/reusable-components/color-picker.js
+++ src/client/components/reusable-components/color-picker.js
@@ -1,6 +1,10 @@
 import React, {useState} from "react";
-import {HuePicker} from "react-color";
+import {GithubPicker} from "react-color";
 import {useSpring, animated, config} from "react-spring";
+
+/* 
+to change default values & styles go to >>
+node_modules\react-color\es\components\github\ */
 
 const ColorPicker = () => {
     const [color, setColor] = useState("#00D2FC");
@@ -9,26 +13,29 @@ const ColorPicker = () => {
     const [showPicker, setShowPicker] = useState(false);
 
     const fadeStyles = useSpring({
-        config: {...config.slow},
-        from: {transformOrigin: "left", transform: "scale(0)"},
+        config: {...config.default},
+        from: {transformOrigin: "left", transform: "scaleX(0)"},
         to: {
-            transform: showPicker ? "scale(1)" : "scale(0)",
+            transform: showPicker ? "scaleX(1)" : "scaleX(0)",
         },
     });
 
     return (
-        <div className={"colorPicker "}>
-            <div
-                onClick={() => setShowPicker(val => !val)}
-                className={"colorPicker__selected"}
-                style={{backgroundColor: color}}
-            />
+        <div className={"k-colorPicker "}>
+            <div className={"k-colorPicker__selectedContainer"}>
+                <div
+                    onClick={() => setShowPicker(val => !val)}
+                    className={"k-colorPicker__selected"}
+                    style={{backgroundColor: color}}
+                />
+            </div>
+
             {console.log(showPicker)}
             <animated.div style={fadeStyles}>
-                <HuePicker
+                <GithubPicker
                     open={open}
                     onClick={() => state => !state}
-                    className={"colorPicker__picker"}
+                    className={"k-colorPicker__picker"}
                     color={color}
                     onChangeComplete={colors => {
                         setColor(colors.hex);
--- src/client/components/reusable-components/loader.js
+++ src/client/components/reusable-components/loader.js
@@ -2,21 +2,16 @@ import React from "react";
 import "../../assets/css/loader.css";
 
 const Loader = () => (
-    <div className={"loader-container"}>
-        <div className={"loader"}>
-            <div className={"loader__bar load__bar--1"} />
-            <div className={"loader__bar load__bar--2"} />
-            <div className={"loader__bar load__bar--3"} />
-            <div className={"loader__bar load__bar--4"} />
-            <div className={"loader__bar load__bar--5"} />
+    <>
+        <div className={"k-loaderContainer"}>
+            <div className={"k-loader"}>
+                <div className={"k-loader__bar k-loader__bar--1"} />
+                <div className={"k-loader__bar k-loader__bar--2"} />
+                <div className={"k-loader__bar k-loader__bar--3"} />
+                <div className={"k-loader__bar k-loader__bar--4"} />
+                <div className={"k-loader__bar k-loader__bar--5"} />
+            </div>
         </div>
-        {/* <div className="loader">
-                <div className="loader__bar load__bar--1-rev"></div>
-                <div className="loader__bar load__bar--2-rev"></div>
-                <div className="loader__bar load__bar--3-rev"></div>
-                <div className="loader__bar load__bar--4-rev"></div>
-                <div className="loader__bar load__bar--5-rev"></div>
-            </div> */}
-    </div>
+    </>
 );
 export default Loader;
