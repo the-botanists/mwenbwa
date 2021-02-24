ce1975b9665c517db196dce1d35eb1caf96561bd
Cédric debroux <71833601+Cedricdebroux@users.noreply.github.com>
2021-02-24 15:45:30 +0100

    top bar log

--- src/client/app.js
+++ src/client/app.js
@@ -1,19 +1,19 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 // import {withRouter} from "react-router";
 import "./assets/css/reset.css";
 import "./assets/style.css";
 import GameBoard from "./components/gameboard";
+import GameBoardLoad from "./components/gameboardlog";
 import Modallog from "../client/components/modalelog";
 // import {divIcon} from "leaflet";
-const burl = "http://localhost";
 if (sessionStorage.getItem("token")) {
     ReactDOM.render(<GameBoard />, document.querySelector("#app"));
 } else {
     ReactDOM.render(
         <BrowserRouter>
-            <Route exact path={`${burl}/user/signup`} />
+            <GameBoardLoad />
             <Modallog />
         </BrowserRouter>,
         document.querySelector("#app"),
---
+++ src/client/components/toplogin.js
@@ -0,0 +1,18 @@
+/* botanists/mwenbwa
+ *
+ * /src/client/components/topbar.js - Hello Component
+ *
+ * started at 11/02/2021
+ */
+
+import * as React from "react";
+
+const TopBarLogin = () => (
+    <nav className={"navbar has-background-primary"}>
+        <div>
+            <h1>{"BATTLE TREE"}</h1>
+        </div>
+    </nav>
+);
+
+export default TopBarLogin;
