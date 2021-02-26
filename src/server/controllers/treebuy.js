2c07180497481cad7697bcd7254da5d078eb727c
Cedric AUDRIT <cedric@audrit.be>
2021-02-26 12:18:07 +0100

    add update console log bgtask + login + register

--- src/server/controllers/treebuy.js
+++ src/server/controllers/treebuy.js
@@ -1,7 +1,24 @@
 import Tree from "../models/tree";
+import Gamelog from "../models/gamelog";
 const Score = require("../models/score");
 const ObjectID = require("mongodb").ObjectID;
 import {nameByRace} from "fantasy-name-generator";
+
+async function updateGameLog(glUser, glAction) {
+    try {
+        const glActionMSG = `Buy a free tree for : ${glAction} 🌿`;
+        const newGamelogObject = new Gamelog({
+            username: glUser,
+            action: glActionMSG,
+        });
+
+        await newGamelogObject.save();
+
+        console.log("Action Add in GAMELOG ...");
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 async function setNewTree(newUserOwner, userColor, onTreeId) {
     console.log("START Set New Tree");
@@ -55,6 +72,7 @@ const buyATree = async (req, res) => {
         console.log(color);
         setNewTree(username, color, treeid);
         updateUserScore(username, treevalue);
+        updateGameLog(username, treevalue);
 
         res.status(200).send(`Tree Buy`);
     } catch (err) {
