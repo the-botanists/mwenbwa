6a48a73c98d8b33add1a900351eb43b8f97ad1eb
KevKsar <cassart.kev@gmail.com>
2021-02-26 12:17:37 +0100

    + request 4 logs

--- src/client/components/gamelog/get-logs.js
+++ src/client/components/gamelog/get-logs.js
@@ -1,75 +1,69 @@
-// import React, {useState, useEffect} from "react";
-// import classnames from "classnames";
-// import {useSpring, animated} from "react-spring";
-// import Loader from "../tools/loader";
+import React, {useState, useEffect} from "react";
+import classnames from "classnames";
+import {useSpring, animated} from "react-spring";
+import Loader from "../tools/loader";
+import moment from "moment";
 
-// function GetLogs() {
-//     const [error, setError] = useState(null);
-//     const [isLoaded, setIsLoaded] = useState(false);
-//     const [items, setItems] = useState([]);
+// import GetLogs from "./get-logs";
 
-//     const scoreAppear = useSpring({
-//         from: {transform: "translateY(150%)"},
-//         to: {transform: "translateY(0)"},
-//         config: {mass: 1, tension: 400, friction: 12},
-//     });
+const GetLogs = () => {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
 
-//     useEffect(() => {
-//         fetch("/api/trees/buy/")
-//             .then(res => res.json())
-//             .then(
-//                 tree => {
-//                     setTimeout(() => {
-//                         setIsLoaded(true);
-//                     }, 3000);
-//                     // debugger
-//                     setItems(
-//                         tree
-//                             .slice(0, 8),
-//                     );
-//                 },
-//                 /*errors must be handled here rather than in
-//                 a catch() block so that we don't swallow exceptions
-//                 due to real bugs in the components.*/
-//                 err => {
-//                     setIsLoaded(true);
-//                     setError(err);
-//                 },
-//             );
-//     }, []);
+    const errorAppear = useSpring({
+        from: {transform: "translateY(150%)"},
+        to: {transform: "translateY(0)"},
+        config: {mass: 1, tension: 400, friction: 12},
+    });
 
-//     if (error) {
-//         return (
-//             <animated.tbody style={scoreAppear}>
-//                 <tr>
-//                     <td>{`Erreur : ${error.message}`}</td>
-//                 </tr>
-//             </animated.tbody>
-//         );
-//     } else if (!isLoaded) {
-//         return (
-//             <Loader />
-//         );
-//     }
-//     return (
-//         <>
-//             <animated.tbody classnames={"k-tbody"} style={scoreAppear}>
-//                 {items.map((item, i) => {
-//                     return (
-//                         <tr className={classnames("k-tableRow")} key={item._id}>
-//                             <td>{item.username}</td>
-//                             <td className={classnames("k-td")}>
+    useEffect(() => {
+        fetch("/api/gamelog/all")
+            .then(res => res.json())
+            .then(
+                logs => {
+                    setTimeout(() => {
+                        setIsLoaded(true);
+                    }, 3000);
+                    setItems(logs.slice(0, 8));
+                },
+                err => {
+                    setTimeout(() => {
+                        setIsLoaded(true);
+                        setError(err);
+                    }, 3000);
+                },
+            );
+    }, []);
 
-//                             </td>
-//                             <td className={classnames("k-td")}>
+    if (error) {
+        return (
+            <animated.tbody style={errorAppear}>
+                <tr>
+                    <td colSpan={"3"}>{`Erreur : ${error.message}`}</td>
+                </tr>
+            </animated.tbody>
+        );
+    } else if (!isLoaded) {
+        return <Loader />;
+    }
 
-//                             </td>
-//                         </tr>
-//                     );
-//                 })}
-//             </animated.tbody>
-//         </>
-//     );
-// }
+    return (
+        <>
+            <tbody classnames={"k-tbody"}>
+                {items.map((item, i) => (
+                    <tr className={classnames("k-tableRow")} key={i.toString()}>
+                        <td>{item.username}</td>
+                        <td className={classnames("k-td")}>{item.action}</td>
+                        <td className={classnames("k-td")}>
+                            {" "}
+                            {moment(item.date).fromNow()}
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </>
+    );
+};
 
-// export default GetLogs;
+export default GetLogs;
